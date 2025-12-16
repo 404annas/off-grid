53ef2bebeb19cd136c67ba2a89e81edf1852c4a1
Muhammad Annas Khan <annasking601@gmail.com>
2025-12-16 16:49:50 +0500

    Done Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import { Facebook, Instagram, Linkedin } from "lucide-react"
 const Footer = () => {
   return (
     <>
-      <div className='bg-white/5 text-center py-2 text-sm flex items-center justify-between px-4'>
+      <div className='bg-white/5 text-center py-2 text-sm flex flex-col gap-2 sm:flex-row items-center justify-between px-4'>
         <p>Developed by <a href="https://techxudo.com/" className='font-normal'>Techxudo</a></p>
+        <div className="flex items-center gap-2">
+          <Facebook size={20} className="cursor-pointer"/>
+          <Instagram size={20} className="cursor-pointer"/>
+          <Linkedin size={20} className="cursor-pointer"/>
+        </div>
         <p>Copyright &copy; 2025</p>
-      </div>
-      <div className="bg-white/5 text-center pb-2 text-sm flex items-center justify-center gap-2 px-4">
-        <Facebook size={20} />
-        <Instagram size={20} />
-        <Linkedin size={20} />
       </div>
     </>
   )
